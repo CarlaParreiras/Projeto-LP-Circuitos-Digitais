03243ba2c273516949881b875d1dd76a1813387a
fernandascovino <fscovinom@hotmail.com>
2016-12-12 23:44:39 -0200

    Update codigo-projeto.rkt

--- codigo-projeto.rkt
+++ codigo-projeto.rkt
@@ -2,11 +2,11 @@
 
 ; -- 3.3.2 Manipulação de queue --
 
-(define (make-queue) (cons '() '()))
+(define (make-queue) (mcons '() '()))
 
-(define (front-ptr queue) (car queue))
+(define (front-ptr queue) (mcar queue))
 
-(define (rear-ptr queue) (cdr queue))
+(define (rear-ptr queue) (mcdr queue))
 
 (define (set-front-ptr! queue item) (set-mcar! queue item))
 
@@ -15,12 +15,12 @@
 (define (front-queue queue)
   (if (empty-queue? queue)
       (error "FRONT called with an empty queue" queue)
-      (car (front-ptr queue))))
+      (mcar (front-ptr queue))))
 
 (define (empty-queue? queue) (null? (front-ptr queue)))
 
 (define (insert-queue! queue item)
-  (let ((new-pair (cons item '())))
+  (let ((new-pair (mcons item '())))
     (cond ((empty-queue? queue)
            (set-front-ptr! queue new-pair)
            (set-rear-ptr! queue new-pair)
@@ -34,7 +34,7 @@
   (cond ((empty-queue? queue)
          (error "DELETE! called with an empty queue" queue))
         (else
-         (set-front-ptr! queue (cdr (front-ptr queue)))
+         (set-front-ptr! queue (mcdr (front-ptr queue)))
          queue)))
 
 ; --//--
@@ -54,21 +54,19 @@
   (if (null? procedures)
       'done
       (begin
-        ((car procedures))
-        (call-each (cdr procedures)))))
+        ((mcar procedures))
+        (call-each (mcdr procedures)))))
 
 (define (make-wire)
   (let ((signal-value 0) (action-procedures '()))
-    (define (set-my-signal! new-value)
+    (define (set-my-signal! new-value) ;toda vez que o wire muda de sinal, chama as ações.
       (if (not (= signal-value new-value))
           (begin (set! signal-value new-value)
                  (call-each action-procedures))
-          'done))
-    
+          'done))    
     (define (accept-action-procedure! proc)
-      (set! action-procedures (cons proc action-procedures))
-      (proc))
-    
+      (set! action-procedures (mcons proc action-procedures))
+      (proc))    
     (define (dispatch m)
       (cond ((eq? m 'get-signal) signal-value)
             ((eq? m 'set-signal!) set-my-signal!)
@@ -76,7 +74,87 @@
             (else (error "Unknown operation - WIRE" m))))
     dispatch))
 
+; AGENDA DE AÇÕES
+
+(define (make-agenda) (mcons 0 '()))
+
+(define (current-time agenda) (mcar agenda)) ;o primeiro valor indica o tempo passado
+
+(define (segments agenda) (mcdr agenda)) ;o segundo valor carrega os segments, que indica as ações que devem ser feitas num determinado tempo
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (set-current-time! agenda time)
+  (set-mcar! agenda time))
+
+(define (set-segments! agenda segments)
+  (set-mcdr! agenda segments))
+
+(define (make-time-segment time queue) ;cada segment possui um tempo e as ações a serem realizadas nesse tempo
+  (mcons time queue))
+
+(define (segment-time s) (mcar s))
+
+(define (segment-queue s) (mcdr s))
+
+(define (first-segment agenda) (mcar (segments agenda)))
+
+(define (rest-segments agenda) (mcdr (segments agenda)))
+
+(define (add-to-agenda! time action agenda) ;adição de ações na agenda
+  (define (belongs-before? segments) ;verifica se a ação a ser adicionada está num tempo anterior ao primeiro segment da agenda
+    (or (null? segments)
+        (< time (segment-time (mcar segments)))))
+  (define (make-new-time-segment time action) ;cria um novo segment
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments) ;adiciona um segment na agenda, no tempo determinado por time
+    (if (= (segment-time (mcar segments)) time)
+        (insert-queue! (segment-queue (mcar segments))
+                       action)
+        (let ((rest (mcdr segments)))
+          (if (belongs-before? rest)
+              (set-mcdr!
+               segments
+               (mcons (make-new-time-segment time action)
+                     (mcdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments! ;se a ação pertence a um tempo anterior, ela é adicionada antes dos segments da agenda
+         agenda
+         (mcons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (begin (delete-queue! q)
+           (if (empty-queue? q)
+               (set-segments! agenda (rest-segments agenda))
+               'pass))))
+
+(define (after-delay delay action) ;adiciona a ação na agenda posteriormente a current-time, num tempo delay
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
 ; BLOCOS LÓGICOS
+
+(define inverter-delay 2) ;os valores para inverter-delay, and-gate-delay e or-gate-delay podem ser escolhidos arbitrariamente para cada circuito
+
+(define and-gate-delay 3)
+
+(define or-gate-delay 5)
 
 (define (inverter input output)
   (define (invert-input)
@@ -121,88 +199,12 @@
       0
       1))
 
-; IMPLEMENTAÇÕES PARA A SIMULAÇÃO
-
-; AFTER-DELAY
-
-(define (after-delay delay action)
-  (add-to-agenda! (+ delay (current-time the-agenda))
-                  action
-                  the-agenda))
-
-; AGENDA DE AÇÕES
-
-(define (make-time-segment time queue)
-  (cons time queue))
-(define (segment-time s) (car s))
-(define (segment-queue s) (cdr s))
-
-(define (make-agenda) (list 0))
-
-(define (current-time agenda) (car agenda))
-
-(define (set-current-time! agenda time)
-  (set-mcar! agenda time))
-
-(define (segments agenda) (cdr agenda))
-
-(define (set-segments! agenda segments)
-  (set-mcdr! agenda segments))
-
-(define (first-segment agenda) (car (segments agenda)))
-
-(define (rest-segments agenda) (cdr (segments agenda)))
-
-(define (empty-agenda? agenda)
-  (null? (segments agenda)))
-
-(define (add-to-agenda! time action agenda)
-  (define (belongs-before? segments)
-    (or (null? segments)
-        (< time (segment-time (car segments)))))
-  (define (make-new-time-segment time action)
-    (let ((q (make-queue)))
-      (insert-queue! q action)
-      (make-time-segment time q)))
-  (define (add-to-segments! segments)
-    (if (= (segment-time (car segments)) time)
-        (insert-queue! (segment-queue (car segments))
-                       action)
-        (let ((rest (cdr segments)))
-          (if (belongs-before? rest)
-              (set-mcdr!
-               segments
-               (cons (make-new-time-segment time action)
-                     (cdr segments)))
-              (add-to-segments! rest)))))
-  (let ((segments (segments agenda)))
-    (if (belongs-before? segments)
-        (set-segments!
-         agenda
-         (cons (make-new-time-segment time action)
-               segments))
-        (add-to-segments! segments))))
-
-(define (remove-first-agenda-item! agenda)
-  (let ((q (segment-queue (first-segment agenda))))
-    (begin (delete-queue! q)
-           (if (empty-queue? q)
-               (set-segments! agenda (rest-segments agenda))
-               'pass))))
-
-(define (first-agenda-item agenda)
-  (if (empty-agenda? agenda)
-      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
-      (let ((first-seg (first-segment agenda)))
-        (set-current-time! agenda (segment-time first-seg))
-        (front-queue (segment-queue first-seg)))))
-
 ; SIMULAÇÃO
 
-; "Sonda" de wire: acompanhamento da mudança
+(define the-agenda (make-agenda))
 
-(define (probe name wire)
-  (add-action! wire
+(define (probe name wire) ;faz a "sonda" de um wire, passa a acompanhar as mudanças no mesmo
+  (add-action! wire ;quando o sinal de wire mudar, o segundo argumento será executado 
                (lambda ()        
                  (newline)
                  (display name)
@@ -211,9 +213,7 @@
                  (display "  New-value = ")
                  (display (get-signal wire)))))
 
-; Executor da agenda: realiza cada procedimento da agenda em sequência
-
-(define (propagate)
+(define (propagate) ;executor da agenda: realiza cada procedimento da agenda em sequência
   (if (empty-agenda? the-agenda)
       'done
       (let ((first-item (first-agenda-item the-agenda)))
@@ -232,10 +232,6 @@
     (and-gate d e s)
     'ok))
 
-; Definindo o circuito
-
-(half-adder input-1 input-2 sum carry)
-
 (define input-1 (make-wire))
 
 (define input-2 (make-wire))
@@ -244,19 +240,13 @@
 
 (define carry (make-wire))
 
+(half-adder input-1 input-2 sum carry)
+
 ; Instanciando a agenda 
-
-(define the-agenda (make-agenda))
-
-(define inverter-delay 2)
-
-(define and-gate-delay 3)
-
-(define or-gate-delay 5)
 
 ; Tracking
 
-(probe 'sum sum) ;Iniciam com time 0 e signal 0
+(probe 'sum sum) ;sum e carry iniciam com time 0 e signal 0
 (probe 'carry carry)
-(set-signal! input-1 1) ;Modificamos o sistema e executamos a agenda
-(propagate)
+(set-signal! input-1 1) ;modificamos o sistema e executamos a agenda
+(propagate) ;ao propagarmos, temos que o sinal de carry será 0 e sum 1, como previsto no circuito
